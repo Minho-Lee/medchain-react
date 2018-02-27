23a231899b7127f4dd4fa024bcdb6e4a1017065e
Minho Lee <minho.lee.93@hotmail.com>
2018-02-27 17:11:12 -0500

    basic validation for registration is now in place.

--- src/js/pages/HomePage.js
+++ src/js/pages/HomePage.js
@@ -3,12 +3,10 @@ import React, { Component } from 'react';
 import LoginSection from '../components/HomePageComponents/LoginSection';
 import WelcomeSection from '../components/HomePageComponents/WelcomeSection';
 
-import '../../scss/HomePage.css';
-
 export default class HomePage extends Component {
 	render() {
 		return (
-			<div className="container">
+			<div className="container" id='main-container'>
 				<WelcomeSection />
 				<LoginSection />
 			</div>
